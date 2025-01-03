443d1402e9b9ae8f69279baf561924d57d6f5d7c
Karolis Balčiūnas <vagabaliukas@gmail.com>
2025-01-04 01:04:21 +0200

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,11 @@ const constants = @import("constants");
 const entities = @import("entities");
 
 pub fn main() !void {
-    rl.initWindow(constants.grid_params.cell_count * constants.grid_params.cell_size, constants.grid_params.cell_count * constants.grid_params.cell_size, "Znake");
+    rl.initWindow(
+        constants.grid_params.cell_count * constants.grid_params.cell_size,
+        constants.grid_params.cell_count * constants.grid_params.cell_size,
+        "Znake",
+    );
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
